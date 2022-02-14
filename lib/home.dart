7c2b381b28394be29c19adf6822ba19df5332517
Deshan <deshanm123@gmail.com>
2022-02-15 00:06:46 +0530

    some changes to gui

--- lib/home.dart
+++ lib/home.dart
@@ -12,62 +12,79 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(20, 80, 20, 0),
-            child: Center(
-              child: Image(
-                width: 100,
-                height: 100,
-                image: AssetImage('images/i.png'),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Pick & go',
-            style: TextStyle(fontSize: 30),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(20, 80, 20, 0),
+                child: Center(
+                  child: Image(
+                    width: 200,
+                    height: 200,
+                    image: AssetImage(
+                        'images/SeekPng.com_delivery-png_1145491.png'),
                   ),
-                  filled: true,
-                  hintStyle: TextStyle(color: Colors.grey[800]),
-                  hintText: "Enter tracking code",
-                  fillColor: Colors.white70),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            width: 380,
-            height: 50,
-            color: Colors.white,
-            child: TextButton(
-              style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          side: const BorderSide(color: Colors.black)))),
-              onPressed: () {
-                Navigator.pushNamed(context, Track.id);
-              },
-              child: const Text(
-                'Track',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
                 ),
               ),
-            ),
-          )
-        ],
+              const SizedBox(height: 20),
+              const Text(
+                'Pick & go',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+                child: Text(
+                  "Pic & Go is Sri Lanka's no 01 unique delivery service. We offering the cheapest, quickest and safest service in door to door delivery.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              // const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      filled: true,
+                      hintStyle: TextStyle(color: Colors.grey[800]),
+                      hintText: "Enter tracking code",
+                      fillColor: Colors.white70),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 380,
+                height: 50,
+                color: Colors.white,
+                child: TextButton(
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              side: const BorderSide(color: Colors.black)))),
+                  onPressed: () {
+                    Navigator.pushNamed(context, Track.id);
+                  },
+                  child: const Text(
+                    'Track',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/track.dart
+++ lib/track.dart
@@ -7,80 +7,116 @@ class Track extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 100),
-        child: Column(
-          children: [
-            const Center(
-              child: Text(
-                'Tracking',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              'Now you can track your shipment',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                ReusableContainer(),
-                SizedBox(width: 10),
-                Text('Order confirmed'),
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 100),
+            child: Column(
+              children: [
+                Container(
+                  child: Image(
+                    image: AssetImage(
+                        'images/SeekPng.com_tracking-icon-png_3818797.png'),
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                const Center(
+                  child: Text(
+                    'Tracking',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text(
+                  'Now you can track your shipment',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(125, 0, 10, 0),
+                      child: ReusableContainer(),
+                    ),
+                    Text('Order confirmed'),
+                  ],
+                ),
+                const Icon(
+                  Icons.arrow_downward,
+                  size: 40,
+                ),
+                // Padding(
+                //   padding:EdgeInsets.fromLTRB(10, 0, 10.0, 0)
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(125, 0, 10, 0),
+                      child: ReusableContainer(),
+                    ),
+                    // SizedBox(width: 45),
+                    Text('Dispatched'),
+                  ],
+                ),
+                // ),
+                const Icon(
+                  Icons.arrow_downward,
+                  size: 40,
+                ),
+
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(125, 0, 10, 0),
+                      child: ReusableContainer(),
+                    ),
+                    // SizedBox(width: 50),
+                    Text('In transist'),
+                  ],
+                ),
+                const Icon(
+                  Icons.arrow_downward,
+                  size: 40,
+                ),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(125, 0, 10, 0),
+                      child: ReusableContainer(),
+                    ),
+                    // SizedBox(width: 42),
+                    Text('Destination'),
+                  ],
+                ),
+                const Icon(
+                  Icons.arrow_downward,
+                  size: 40,
+                ),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(125, 0, 10, 0),
+                      child: ReusableContainer(),
+                    ),
+                    Text(
+                      'Delivered',
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 40),
               ],
             ),
-            const Icon(
-              Icons.arrow_downward,
-              size: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                ReusableContainer(),
-                SizedBox(width: 45),
-                Text('Dispatched'),
-              ],
-            ),
-            const Icon(
-              Icons.arrow_downward,
-              size: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                ReusableContainer(),
-                SizedBox(width: 50),
-                Text('In transist'),
-              ],
-            ),
-            const Icon(
-              Icons.arrow_downward,
-              size: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                ReusableContainer(),
-                SizedBox(width: 42),
-                Text('Destination'),
-              ],
-            ),
-            const Icon(
-              Icons.arrow_downward,
-              size: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                ReusableContainer(),
-                SizedBox(width: 50),
-                Text('Delivered'),
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
